a76727c3f87e63bd838a6e320dd758694511d4fb
Mehdi Zibout <mehdizibout@gmail.com>
2023-01-19 15:48:36 +0100

    Tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { Jost } from "@next/font/google";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+const jost = Jost({ subsets: ["latin"] });
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={jost.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,46 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      tablet: "670px",
+      desktop: "1200px",
+    },
+    colors: {
+      white: "#fff",
+      purple: "#AD1FEA",
+      blue: "#4661E6",
+      "dark-blue": "#4661E6",
+      "dark-blue-darker": "#3A4374",
+      "light-grey": "#F2F4FF",
+      "light-grey-lighter": "#F7F8FD",
+      orange: "#F49F85",
+      "light-blue": "#62BCFA",
+      "greyish-blue": "#647196",
+    },
+    fontSize: {
+      h1: [
+        "1.5rem",
+        { fontWeight: 700, lineHeight: "2.18rem", letterSpacing: "-0.33px" },
+      ],
+      h2: [
+        "1.25rem",
+        { fontWeight: 700, lineHeight: "1.81rem", letterSpacing: "-0.25px" },
+      ],
+      h3: [
+        "1.125rem",
+        { fontWeight: 700, lineHeight: "1.625rem", letterSpacing: "-0.25px" },
+      ],
+      h4: [
+        "0.875rem",
+        { fontWeight: 700, lineHeight: "1.25rem", letterSpacing: "-0.2px" },
+      ],
+      body1: ["1rem", { lineHeight: "1.43rem" }],
+      body2: ["0.94rem", { lineHeight: "1.375rem" }],
+      body3: ["0.81rem", { lineHeight: "1.18rem" }],
+    },
+    fontFamily: {
+      sans: ["Jost", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
